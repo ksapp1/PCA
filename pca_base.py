f62a6a72f124356244cbc73c74be48196c25b93e
Kayla Sapp <ksapp1@users.noreply.github.com>
2024-01-17 15:01:34 -0500

    Update pca_base.py

--- pca_base.py
+++ pca_base.py
@@ -1,5 +1,4 @@
 #!/opt/local/bin/python                                                                                                 
-
 # Load the necessary packages: numpy, scipy, MDAnalysis, sklearn, etc.                                                  
 import numpy as np
 import MDAnalysis as mda
@@ -12,10 +11,7 @@ import matplotlib.pyplot as plt
 from scipy import stats
 
 np.random.seed(0)  # set the seed for the random number                                                                 
-
-
 # # Load the trajectory using MDAnalysis                                                                                
-
 path = "./vdac1h_pc/" # define the path to the set of files to load using the MDA Universe                              
 file_name = "vdac1-dopc" # base file name to load                                                                       
 traj = [] # create a list of the trajectory files to be loaded                                                          
@@ -29,7 +25,6 @@ u = mda.Universe(path + file_name + '.psf', traj) # load the trajectory using th
 # Align the trajectory.                                                                                                 
 # Create an AtomGroup containing the desired atoms from the simulation that we want to perform PCA on. 
 # This example uses the beta barrel of VDAC without the N terminus and loops                                                    
-
 n_term = " and not (resid 1:25)"
 loops = [34,38,48,52,64,68,76,79,88,94,104,108,120,122,132,135,146,148,158,163,175,177,185,188,197,201,212,215,228,230,238,241,252,254,265,273]
 ex_ids = n_term
@@ -40,10 +35,9 @@ BetaBarrel = u.select_atoms("backbone" + ex_ids)
 # store the AtomGroup information in an data frame [resnames, ids, atom names]                                          
 Barrel_df = pd.DataFrame(np.array([BetaBarrel.resnames, BetaBarrel.resids, BetaBarrel.names]).T,columns=['resname', 'resid', 'atom'])
 
-# # Run PCA on VDAC using MDAnalysis                                                                                    
+# # Run PCA on VDAC using MDAnalysis                          
 # Use the select argument to select the desired atoms (idealy the same as the AtomGroup above)                          
 # The components are stored in results.p_components with shape (natoms*3, natoms*3)                                     
-
 pca_vdac = pca.PCA(u, select="backbone" + ex_ids, align=True).run()
 
 # Determine the center of mass distance for each atom.
@@ -63,7 +57,6 @@ n_pcs = np.where(pca_vdac.cumulated_variance > 0.9)[0][0]
 
 # Transform the AtomGroup into reduced space (the weights over each component). 
 # The output has shape (n_frames, n_components)                                                                                                                  
-
 pca_space = pca_vdac.transform(BetaBarrel, n_pcs)
 
 # project the original traj onto each of the first component to visualize the motion. Can repeat for each component     
@@ -72,7 +65,6 @@ projected = np.outer(pca_space[:, 0], pca_vdac.p_components[:, 0]) + pca_vdac.me
 coordinates = projected.reshape(len(pca_space[:, 0]), -1, 3)
 
 # We can store information about each component as the beta value in the pdb. Here we use the raidial projection of the component                                                                                                              
-
 comp = pca_vdac.p_components[:,0].reshape(len(BetaBarrel),3)
 beta = dr[:,0]*comp[:,0] + dr[:,1]*comp[:,1]
 bfactor_norm = beta/(2*np.absolute(beta).max()) + 0.5
@@ -81,7 +73,6 @@ new_data = {'beta_1':beta, 'beta_norm_1':bfactor_norm}
 Barrel_df.assign(**new_data)
 
 # Can create a new universe of these projected coordinates                                                              
-
 proj1 = mda.Merge(BetaBarrel)
 proj1.load_new(coordinates, order="fac")
 proj1.add_TopologyAttr('tempfactors')
